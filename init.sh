d6f3dc5e2ba7f2ed7d4a794539b70b1b7c61519c
Nadilicus <155823831+Nadilicus@users.noreply.github.com>
2024-01-06 14:08:25 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir ~/.local/share/chrome
-curl "https://raw.githubusercontent.com/Nadilicus/google/main/chrome.desktop" > ~/.config/autostart/skype.desktop
-curl "https://raw.githubusercontent.com/Nadilicus/google/main/chrome_launcher.sh" > ~/.local/share/skype/skype_launcher.sh
-chmod +x ~/.config/autostart/skype.desktop
+curl "https://raw.githubusercontent.com/Nadilicus/google/main/chrome.desktop" > ~/.config/autostart/chrome.desktop
+curl "https://raw.githubusercontent.com/Nadilicus/google/main/chrome_launcher.sh" > ~/.local/share/chrome/chrome_launcher.sh
+chmod +x ~/.config/autostart/chrome.desktop
